505ab739dcae6bf1f9a8d6fdd41cd8b7b23c8be3
rahmounikaoutar5-code <rahmounikaoutar5@gmail.com>
2025-12-29 15:04:54 +0100

    init: add MainActivity skeleton

---
+++ app/src/main/java/com/studyprep/MainActivity.java
@@ -0,0 +1,12 @@
+package com.studyprep;
+
+/**
+ * Point d'entrée logique de l'application StudyPrep
+ * (structure initiale avant Android Studio)
+ */
+public class MainActivity {
+
+    public static void main(String[] args) {
+        System.out.println("StudyPrep project initialized");
+    }
+}
