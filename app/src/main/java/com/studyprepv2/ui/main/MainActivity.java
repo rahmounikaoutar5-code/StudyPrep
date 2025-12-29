f511f07c80b6b1c27dd66c042a1a179cb3aea339
rahmounikaoutar5-code <rahmounikaoutar5@gmail.com>
2025-12-29 15:44:43 +0100

    refactor: add MainActivity in ui/main

---
+++ app/src/main/java/com/studyprepv2/ui/main/MainActivity.java
@@ -0,0 +1,14 @@
+package com.studyprepv2.ui.main;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import com.studyprepv2.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
